a1cac1c52bef72aac8527a21259e661589c3f6a7
brandon santos <brandonx1123@gmail.com>
2021-02-16 22:42:22 -0500

    MVP Done for flex :]

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,6 +1,6 @@
-import React from 'react';
-
-const Total = props => {
+import React from "react";
+import { connect } from "react-redux";
+const Total = (props) => {
   return (
     <div className="content">
       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
@@ -8,4 +8,8 @@ const Total = props => {
   );
 };
 
-export default Total;
+const mapStateToProps = (state) => {
+  const { additionalPrice, car } = state;
+  return { car, additionalPrice };
+};
+export default connect(mapStateToProps)(Total);
